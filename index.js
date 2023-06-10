b9a8366ca1b1d2c94985746b96a88dd2c7f79336
Mr. Jay Patel <108916822+Jay-Vekariya@users.noreply.github.com>
2023-06-10 11:19:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+let myLeads = []
+// let oldLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deletebtn = document.getElementById("delete-btn")
+const leadsfromlocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const savebtn = document.getElementById("tab-btn")
+
+//check the condition is truthy or falsy...
+if(leadsfromlocalStorage) {
+    myLeads = leadsfromlocalStorage
+    render(myLeads)
+}
+// Grab the URL of the current active tab...
+savebtn.addEventListener("click", function(){ 
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
+//hardcoded functions code...
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems   
+}
+//for delete all values...
+deletebtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+//save the inut of the input text...
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
+
